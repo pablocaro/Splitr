4fb3f74ce9f40214fb3e8c7491d70ecdcc0efa7f
pablocaro <caro.pablo@gmail.com>
2016-07-31 08:35:35 -0700

    Update

--- Splitr/ViewController.swift
+++ Splitr/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipControl.alpha = 0
+        tipControl.center.y = 305
+        var editing = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dismiss(sender: AnyObject) {
+              view.endEditing(true)
+    }
 
+    
+    @IBAction func showTipControl(sender: AnyObject) {
+        
+        
+        
+        if editing == false {
+            
+            UIView.animateWithDuration(0.4, animations: {
+                self.tipControl.alpha = 1
+                self.tipControl.center.y = 295
+                
+            })
+            
+            editing = true
+        }
+    
+    }
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        tipLabel.text = String(format: "%.2f",tip)
+        totalLabel.text = String(format: "%.2f",total)
+        
+        
+        
+    }
 }
 
